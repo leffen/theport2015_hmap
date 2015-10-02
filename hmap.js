456bb75695395ffedcfb4917278a054856c266f2
leffen <leffen@gmail.com>
2015-10-02 17:23:31 +0200

    Initial commit

---
+++ hmap.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var http = require('http');
+var PF = require('pathfinding');
+var matrix = [
+  [0, 0, 0, 0, 0,0,0,0,0,0],
+  [0, 0, 0, 0, 0,0,0,0,0,0],
+  [0, 0, 1, 1, 1,1,1,1,0,0],
+  [0, 0, 1, 1, 0,0,0,1,0,0],
+  [0, 0, 1, 1, 0,0,0,1,0,0],
+  [0, 0, 1, 1, 0,0,0,1,0,0],
+  [0, 0, 1, 1, 0,0,0,1,0,0],
+  [0, 0, 1, 1, 1,1,1,1,0,0],
+  [0, 0, 0, 0, 0,0,0,0,0,0],
+  [0, 0, 0, 0, 0,0,0,0,0,0]
+];
+
+var markers = [
+  [2,0,"M1"],
+  [6,0,"M2"],
+  [9,2,"M3"],
+  [9,7,"M4"],
+  [6,9,"M5"],
+  [2,9,"M6"]
+];
+
+var grid = new PF.Grid(matrix);
+// Express and middlewares
+var app = express();
+
+
+// Actual query
+app.get('/directions/:location', function (req, res, next) {
+  // console.log(req);
+  console.log(req.params);
+
+  location = req.params.location;
+  direction=req.query.direction;
+  destination=req.query.destination;
+
+  console.log(location,direction,destination);
+    res.json([[1,2],[2,3]]);
+});
+
+// Standalone server setup
+var port = process.env.PORT || 3001;
+http.createServer(app).listen(port, function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Listening on http://localhost:' + port);
+  }
+});
