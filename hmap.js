8fcec3c930884d445acd9648af7247c9e8693317
leffen <leffen@gmail.com>
2015-10-02 17:39:56 +0200

    Adding path finding

--- hmap.js
+++ hmap.js
@@ -2,28 +2,30 @@ var express = require('express');
 var http = require('http');
 var PF = require('pathfinding');
 var matrix = [
-  [0, 0, 0, 0, 0,0,0,0,0,0],
-  [0, 0, 0, 0, 0,0,0,0,0,0],
-  [0, 0, 1, 1, 1,1,1,1,0,0],
-  [0, 0, 1, 1, 0,0,0,1,0,0],
-  [0, 0, 1, 1, 0,0,0,1,0,0],
-  [0, 0, 1, 1, 0,0,0,1,0,0],
-  [0, 0, 1, 1, 0,0,0,1,0,0],
-  [0, 0, 1, 1, 1,1,1,1,0,0],
-  [0, 0, 0, 0, 0,0,0,0,0,0],
-  [0, 0, 0, 0, 0,0,0,0,0,0]
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
+  [0, 0, 1, 1, 0, 0, 0, 1, 0, 0],
+  [0, 0, 1, 1, 0, 0, 0, 1, 0, 0],
+  [0, 0, 1, 1, 0, 0, 0, 1, 0, 0],
+  [0, 0, 1, 1, 0, 0, 0, 1, 0, 0],
+  [0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
 
 var markers = [
-  [2,0,"M1"],
-  [6,0,"M2"],
-  [9,2,"M3"],
-  [9,7,"M4"],
-  [6,9,"M5"],
-  [2,9,"M6"]
+  [2, 0, "M1"],
+  [6, 0, "M2"],
+  [9, 2, "M3"],
+  [9, 7, "M4"],
+  [6, 9, "M5"],
+  [2, 9, "M6"]
 ];
 
 var grid = new PF.Grid(matrix);
+var finder = new PF.AStarFinder();
+
 // Express and middlewares
 var app = express();
 
@@ -32,13 +34,16 @@ var app = express();
 app.get('/directions/:location', function (req, res, next) {
   // console.log(req);
   console.log(req.params);
+  location = req.params.location; // Maps to hospital
+  position = req.query.position;
+  direction = req.query.direction;  //
+  destination = req.query.destination;
 
-  location = req.params.location;
-  direction=req.query.direction;
-  destination=req.query.destination;
+  console.log(location,position, direction, destination);
 
-  console.log(location,direction,destination);
-    res.json([[1,2],[2,3]]);
+  var path = finder.findPath(1, 2, 4, 2, grid);
+
+  res.json([[1, 2], [2, 3]]);
 });
 
 // Standalone server setup
