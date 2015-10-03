2acd67ac1e35726a5b20bf535599cddcfaeef609
leffen <leffen@gmail.com>
2015-10-03 11:57:30 +0200

    WIP

--- source/map_enhancer.js
+++ source/map_enhancer.js
@@ -1,17 +1,24 @@
 var PF = require('pathfinding');
 
-module.exports = function(grid,markers) {
-  var originalGrid = new PF.Grid(grid);
+var grid_map1 = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
+  [0, 0, 1, 1, 0, 0, 0, 1, 0, 0],
+  [0, 0, 1, 1, 0, 0, 0, 1, 0, 0],
+  [0, 0, 1, 1, 0, 0, 0, 1, 0, 0],
+  [0, 0, 1, 1, 0, 0, 0, 1, 0, 0],
+  [0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+
+
+module.exports.load_grid = function (location) {
+  return new PF.Grid(grid_map1);
+};
+
+module.exports.resolve = function (x1, y1, x2, y2, grid) {
   var finder = new PF.AStarFinder();
-
-  var factory = {};
-
-  factory.resolve =  function (x1, y1, x2, y2, grid) {
-    return finder.findPath(x1, y1, x2, y2, grid);
-  };
-  factory.load_grid = function (location) {
-    return originalGrid.clone();
-  };
-
-  return factory;
+  return finder.findPath(x1, y1, x2, y2, grid);
 };
